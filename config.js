32a78f40260d9620477d73fc95787b6cdb557f6e
chandrawiguna99 <chndrwgnn66@gmail.com>
2022-06-14 21:20:21 +0700

    faxbot

---
+++ config.js
@@ -0,0 +1,72 @@
+const fs = require("fs")
+const chalk = require("chalk")
+
+// Other
+global.thum = "https://telegra.ph/file/61e4db12aadcc07725602.jpg"
+global.log0 = fs.readFileSync("./media/fenzo.jpg")
+global.err4r = "https://telegra.ph/file/61e4db12aadcc07725602.jpg"
+global.owner = ['6285861009691','6281211423262','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524','6285778815524']
+global.lolhuman = "KaysaS"
+global.rkyt = []
+global.banUser = ['6281366679021@s.whatsapp.net']
+global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
+global.wlcm = []
+global.gcrevoke = ['19054091487-1424650263@g.us']
+global.packname = "Punya"
+global.packname2 = "BabyBot++"
+global.author = "Chandra"
+global.sessionName = "faxx"
+global.namebot = "Baby Bot"
+global.linkgrupss = "https://chat.whatsapp.com/"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '*Prosessss kack,* _FOLLOW INSTAGRAM OWNER_ *_@chndrwgnn__*',
+    linkm: 'Linknya Mana Kak?',
+    ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+// Beli Di Zenz Yo
+global.APIs = {
+	zenz: 'https://api.zeeoneofc.xyz',
+}
+
+// Isi Api Mu Di sini
+global.APIKeys = {
+	'https://api.zeeoneofc.xyz': '49tEH5me',
+}
+//Thank Fax Bot
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
